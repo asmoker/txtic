cd9a3888c145c1d6ef47da514ef3437e0d59df06
asmoker <blog.smoker.cc@gmail.com>
2023-03-02 13:54:25 +0800

    Removed unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "top.threep.plugin"
-version = "0.5.1"
+version = "0.5.2"
 
 repositories {
     mavenCentral()
@@ -16,7 +16,7 @@ intellij {
     version.set("2022.1.4")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf("java"))
+    plugins.set(listOf())
 }
 
 tasks {
